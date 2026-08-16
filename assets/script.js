f82d823d2701fb2201b4836ef81162039a34730b
Mark <msntndr.ms@gmail.com>
2026-08-16 12:00:22 -0400

    New styling theme

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -8,20 +8,20 @@ function listTopics() {
 }
 
 function selectTopic() {
-  if (randomTopic === 'HTML') {
+  if (randomTopic === "HTML") {
     console.log("Let's study HTML!");
-  } else if (randomTopic === 'CSS') {
+  } else if (randomTopic === "CSS") {
     console.log("Let's study CSS!");
-  } else if (randomTopic === 'Git') {
+  } else if (randomTopic === "Git") {
     console.log("Let's study Git!");
-  } else if (randomTopic === 'JavaScript') {
+  } else if (randomTopic === "JavaScript") {
     console.log("Let's study JavaScript!");
   } else {
-    console.log('Please try again!');
+    console.log("Please try again!");
   }
 }
 
-console.log('Here are the topics we learned through Prework:');
+console.log("Here are the topics we learned through Prework:");
 listTopics();
-console.log('Which topic should we study first?');
+console.log("Which topic should we study first?");
 selectTopic();
